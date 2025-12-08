8f063de1bd70d85f257f2f35161fd6a02a48b9e9
Michael Gruenstaeudl <7493028+michaelgruenstaeudl@users.noreply.github.com>
2025-12-08 14:12:30 -0600

    cleanup

--- README.md
+++
@@ -1,2 +0,0 @@
-# My Curriculum Vitae in LaTeX
-Developing a professional academic Curriculum Vitae in LaTeX takes times; this is where I do it. 
--- currentCV_compile.sh
+++
@@ -1,6 +0,0 @@
-pdflatex currentCV
-bibtex currentCV
-pdflatex currentCV
-pdflatex currentCV
-
-#pandoc currentCV.tex --bibliography=bibtex/CollectedPublications_MichaelGruenstaeudl_2025_07_21.bib -s -o myfile.html
